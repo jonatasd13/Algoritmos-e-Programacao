d2baf22f3a57a34c5f13f2b42cbd9f0daaeb4b81
Jonatas <90988676+jonatasd13@users.noreply.github.com>
2021-11-12 16:08:30 -0300

    Add files via upload

---
+++ POOS2/Exercicios_Aula/src/Exercicio10/FuncDTO.java
@@ -0,0 +1,21 @@
+package Exercicio10;
+
+public class FuncDTO {
+    private int codigo, telefone;
+    
+    public int getCodigo(){
+        return codigo;
+    }
+    
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+    
+    public int getTelefone(){
+        return telefone;
+    }
+    
+    public void setTelefone(int telefone){
+        this.telefone = telefone;
+    }
+}
