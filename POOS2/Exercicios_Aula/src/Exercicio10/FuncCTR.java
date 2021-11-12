625c21eb479df53712e60144bd4874dbe8e02494
Jonatas <90988676+jonatasd13@users.noreply.github.com>
2021-11-12 16:07:47 -0300

    exercicios

--- POOS2/Exercicios_Aula/src/Exercicio10/FuncCTR.java
+++
@@ -1,9 +0,0 @@
-package Exercicio10;
-
-public class FuncCTR {
-    FuncDAO funcDAO = new FuncDAO();
-    
-    public String mostrar(FuncDTO funcDTO){
-        return funcDAO.mostrar(funcDTO);
-    }
-}
